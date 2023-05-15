2750e22a012beb6514681bd1b8b3a39decf46fef
Pavlo <kachurovskiy.p@gmail.com>
2023-05-15 17:59:20 +0200

    add build

---
+++ js/settings.js
@@ -0,0 +1,48 @@
+export const select = {
+  containerOf: {
+    menu: '.header__menu-list',
+    page: '.page-wrapper',
+    finder: '.finder-wrapper',
+    alert: '.alert',
+    button: '.finder__button',
+    finderTitle: '.finder__title',
+    gridSell: '.cell'
+  },
+  menu: {
+    link: '.header__menu-link',
+    mainPageId: 'about'
+  }
+};
+
+export const classNames = {
+  hidden: 'hidden',
+  gridCell: 'cell',
+  gridChoosed: 'choosed',
+  start: 'start',
+  end: 'end'
+};
+
+export const settings = {
+  countCols: 10,
+  countRows: 10,
+  step: {
+    drawing: 'drawing',
+    poinst: 'points',
+    calc: 'calc'
+  }
+};
+
+export const message = {
+  break: 'the road cannot have breaks',
+  points: 'you have chosen points',
+  outRange: 'you point is out of choosed road',
+  startPoint: 'this is start point',
+  endPoint: 'this is end point',
+  pic: 'please pic start and finish',
+  drawTitle: 'draw routes',
+  pointsTitle: 'pic start and finish',
+  calcTitle: 'the best route is...',
+  drawBtnTitle: 'finish drawing',
+  pointsBtnTitle: 'compute',
+  calcBtnTitle: 'start again'
+};
